ee040fb22e9d7bccd210e8024e8d2d1a3ff5b125
bananvyhe <loadonden@yahoo.com>
2020-07-06 23:24:53 +0500

    1

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq::Extensions.enable_delay!
 Sidekiq.configure_server do |config|
-	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: "sidekiq_farmspot" }
+	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: "sidekiq_app_name" }
 end
 
 Sidekiq.configure_client do |config|
-	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: "sidekiq_farmspot" }
+	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_farm', namespace: "sidekiq_app_name" }
 end
