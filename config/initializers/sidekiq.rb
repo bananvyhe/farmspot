92ad8f2c62f4e7f7b4fbb6b4b57cc88567d78df4
bananvyhe <loadonden@yahoo.com>
2020-07-06 14:47:46 +0500

    has worked on dev server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq::Extensions.enable_delay!
 Sidekiq.configure_server do |config|
-	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_farmspot_#{Rails.env}" }
+	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_farmspot" }
 end
 
 Sidekiq.configure_client do |config|
-	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_farmspot_#{Rails.env}" }
+	config.redis = { url: 'redis://127.0.0.1:6379/sidekiq_test', namespace: "sidekiq_farmspot" }
 end
